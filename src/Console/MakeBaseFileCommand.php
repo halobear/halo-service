76a516ddb5ed695612f2fb868e7a372ae409a57a
管少秋 <94600115@qq.com>
2020-05-29 23:31:35 +0800

    feat: 添加自定义命令

--- src/Console/MakeBaseFileCommand.php
+++ src/Console/MakeBaseFileCommand.php
@@ -4,15 +4,18 @@ namespace HaloService\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Facades\File;
 
-class MakeBaseFileCommand extends GeneratorCommand
+class MakeBaseFileCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:base_file';
+    protected $signature = 'make:base_file {name : Class (singular) for example User}';
+
+    protected $type = 'BaseOperation';
 
     /**
      * The console command description.
@@ -28,14 +31,61 @@ class MakeBaseFileCommand extends GeneratorCommand
      */
     public function handle()
     {
-        $this->info('111122222');
+        $name = $this->argument('name');
+
+        $this->controller($name);
+        $this->model($name);
+
+        \File::append(base_path('routes/api.php'), 'Route::resource(\'' . strtolower($name) . "', '{$name}Controller');");
+
+        $this->info($this->type . ' created successfully.');
+    }
+
+    /**
+     * 获取传入的类名
+     *
+     * @return string
+     */
+    protected function getNameInput()
+    {
+        return trim($this->argument('name'));
     }
 
     /**
      * @inheritDoc
      */
-    protected function getStub()
+    protected function getStub($type)
     {
-        // TODO: Implement getStub() method.
+        return file_get_contents(__DIR__ . "/stubs/$type.stub");
+    }
+
+    protected function model($name)
+    {
+        $modelTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Model')
+        );
+
+        file_put_contents(app_path("/{$name}.php"), $modelTemplate);
+    }
+
+    protected function controller($name)
+    {
+        $controllerTemplate = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+            ],
+            [
+                $name,
+                strtolower($name),
+                strtolower($name),
+            ],
+            $this->getStub('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $controllerTemplate);
     }
 }
